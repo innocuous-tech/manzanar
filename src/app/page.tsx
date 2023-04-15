a72870905786fbaeacda7d5d7032f7b2ec791288
Kyle Holmberg <kylemh.email@gmail.com>
2023-04-16 02:15:06 +0530

    update copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
         <h1 className="text-6xl sm:text-8xl">Project Manzanar</h1>
 
         <p className="text-xl text-[#DEDBCD] px-4">
-          An exploratory project into Manzanar leveraging Inworld.
+          An exploratory project into the experience of Manzanar, leveraging Inworld AI™.
           <br />
           <span className="font-semibold">Coming soon...</span>
         </p>
