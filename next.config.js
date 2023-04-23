38f104e2a02d67a0452e35b09761eb539d90dc4b
Kyle Holmberg <kylemh.email@gmail.com>
2023-04-24 02:33:56 +0530

    setup svgr

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const withSvgr = require('@newhighsco/next-plugin-svgr');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  excludeDefaultMomentLocales: true,
+  poweredByHeader: false,
   experimental: {
     appDir: true,
+    legacyBrowsers: false,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = withSvgr(nextConfig);
