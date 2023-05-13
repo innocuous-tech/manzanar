f7ba71c465b45b8bba7c25c583496b02cbc8aef6
Kyle Holmberg <kylemh.email@gmail.com>
2023-05-14 02:01:38 +0300

    fix component design problems

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -8,28 +8,42 @@ import { forwardRef } from 'react';
 
 export const DialogContent = forwardRef<
   HTMLDivElement,
-  DialogPrimitive.DialogContentProps
->(({ children, ...props }, forwardedRef) => (
+  DialogPrimitive.DialogContentProps & {
+    variant?: 'default' | 'full-screen-overlay';
+  }
+>(({ children, variant = 'default', ...props }, forwardedRef) => (
   <DialogPrimitive.Portal>
     <DialogPrimitive.Overlay className="fixed inset-0 data-[state=closed]:animate-toNoOpacity data-[state=open]:animate-toFullOpacity" />
 
     <DialogPrimitive.Content
       {...props}
-      className={clsx(
-        props.className,
-        'fixed left-[50%] top-[50%] flex h-full w-full translate-x-[-50%] translate-y-[-50%] flex-col-reverse items-end gap-6 rounded-xl bg-darkBrownOverlay p-12 shadow-overlay outline-none data-[state=closed]:animate-dialogContentHide data-[state=open]:animate-dialogContentShow sm:block sm:h-[calc(100%-8rem)] sm:w-[calc(100%-18rem)] sm:rounded-xl md:w-[calc(100%-24rem)]',
-      )}
+      className={clsx(props.className, {
+        ['fixed left-[50%] top-[50%] flex h-full w-full translate-x-[-50%] translate-y-[-50%] flex-col-reverse items-end gap-6 rounded-xl bg-darkBrownOverlay p-12 shadow-overlay outline-none data-[state=closed]:animate-dialogContentHide data-[state=open]:animate-dialogContentShow sm:block sm:h-[calc(100%-8rem)] sm:w-[calc(100%-18rem)] sm:rounded-xl md:w-[calc(100%-24rem)]']:
+          variant === 'default',
+        ['fixed inset-0 grid h-screen w-screen place-items-center bg-menuOverlay']:
+          variant === 'full-screen-overlay',
+      })}
       ref={forwardedRef}
     >
-      <div className="custom-scrollbar h-full overflow-y-auto pr-6 text-cream">
+      <div
+        className={clsx('overflow-y-auto text-cream', {
+          ['custom-scrollbar h-full pr-6']: variant === 'default',
+        })}
+      >
         {children}
       </div>
 
       <DialogPrimitive.Close asChild>
         <IconButton
-          className="sm:absolute sm:-right-[7rem] sm:top-0 sm:shadow-overlay md:-right-[8rem]"
+          className={clsx({
+            ['sm:absolute sm:-right-[7rem] sm:top-0 sm:shadow-overlay md:-right-[8rem]']:
+              variant === 'default',
+            ['absolute right-8 top-8 shadow-overlay']:
+              variant === 'full-screen-overlay',
+          })}
           label="Close Dialog"
           hasTooltip={false}
+          variant={variant === 'default' ? 'transparent' : 'solid'}
         >
           <XIcon />
         </IconButton>
--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -7,24 +7,33 @@ export const IconButton = ({
   className,
   label,
   hasTooltip = true,
+  variant = 'transparent',
   ...props
 }: Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'children' | 'aria-label'> & {
   children: JSX.Element;
   label: string;
+
   /** @default true */
   hasTooltip?: boolean;
+
+  /** @default 'transparent' */
+  variant?: 'transparent' | 'solid';
 }) => {
+  const isTransparent = variant === 'transparent';
   const button = (
     <Button
       className={clsx(
         className,
+        {
+          ['border-none bg-darkBrownOverlay shadow-overlay disabled:border-none']:
+            !isTransparent,
+        },
         'sm:[&>svg]:h-10',
         'sm:[&>svg]:w-10',
         '[&>svg]:h-8',
         '[&>svg]:w-8',
         '!sm:p-6',
         '!p-4',
-        'bg-darkBrownOverlay',
       )}
       aria-label={label}
       {...props}
