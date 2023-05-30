e6004b4d2cd980015fe83e777a41245820100304
Kyle Holmberg <kylemh.email@gmail.com>
2023-05-30 19:26:56 +0300

    trim ichiro statements visibly

--- src/app/(experience)/_components/IchiroStatement.tsx
+++ src/app/(experience)/_components/IchiroStatement.tsx
@@ -4,7 +4,7 @@ export const IchiroStatement = ({ children }: PropsWithChildren<{}>) => {
   return (
     <>
       <span className="typography-name text-ichiro">Ichiro:</span>
-      <p>{children}</p>
+      <p>{typeof children === 'string' ? children.trim() : children}</p>
     </>
   );
 };
