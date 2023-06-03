b2bd48e76264c4e76eec09f0b0073be76ab00ae3
Kyle Holmberg <kylemh.email@gmail.com>
2023-06-03 21:55:46 +0300

    ditch edge config

--- src/app/api/initInworld/route.ts
+++ src/app/api/initInworld/route.ts
@@ -2,8 +2,6 @@ import { InworldClient } from '@inworld/nodejs-sdk';
 
 export const revalidate = 60;
 export const dynamic = 'force-dynamic';
-export const runtime = 'edge';
-export const preferredRegion = 'global';
 
 export async function GET() {
   const client = new InworldClient().setApiKey({
