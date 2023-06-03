a39c5766fb7b898712e652e1ebee28f35cbf4539
Kyle Holmberg <kylemh.email@gmail.com>
2023-06-03 19:09:12 +0300

    hopefully fix inworld api access

--- next.config.js
+++ next.config.js
@@ -9,6 +9,20 @@ const nextConfig = {
     appDir: true,
     legacyBrowsers: false,
   },
+  async headers() {
+    return [
+      {
+        source: '/api/initInworld',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'max-age=240, stale-while-revalidate=60',
+          },
+        ],
+      },
+    ];
+  },
+
 };
 
 module.exports = withSvgr(nextConfig);
